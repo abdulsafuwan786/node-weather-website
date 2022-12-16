da27341811f6d999eb0eafd0e88f3df75062e187
unknown <abdulsafuwan@gmail.com>
2022-12-16 23:45:58 +0530

    Added comment to aknowledge the app.js for front end

--- public/js/app.js
+++ public/js/app.js
@@ -2,9 +2,9 @@ console.log('Client side javascript file is loaded!')
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+//Javascript for frontend page 
 const messageOne = document.querySelector('#message-1');
 const messageTwo = document.querySelector('#message-2');
-
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
